9631a2aa619fe0bfd6ac51b9648be1e3b2d04faa
g1zman <matvey.guzukin@gmail.com>
2024-09-20 19:56:30 +0300

    update 1

--- my1.py
+++ my1.py
@@ -22,18 +22,25 @@ with open("ranData.txt","r",encoding="utf-8") as file:
             index = int(input("Enter the required index:"))
             values_at_index = [number_list[index] for number_list in data.values() if len(number_list) > index]
             if values_at_index:
-                lowcost = min(values_at_index)
-                highcost = max(values_at_index)
-                print(f"Highest price on index {index}:", highcost, "on market:",)
-                print(f"Lowest price on index {index}:", lowcost, "on market:",)
-                print(f"Potential spread on index {index}:", highcost - lowcost)
+                minn = values_at_index[0]
+                for d in values_at_index[1:]:
+                    if d <= minn:
+                        minn = d
+            if values_at_index:
+                maxx = 0
+                for l in values_at_index:
+                    if l > maxx:
+                        maxx = l
+                print(f"Highest price on index {index}:", maxx, "on market:",)
+                print(f"Lowest price on index {index}:", minn, "on market:",)
+                print(f"Potential spread on index {index}:", maxx - minn)
             else:
                 print(f"Insufficient data on index {index}")
                 # классы читать
                 # массивы
                 # git + github DONE
                 # add time - index correlation. add hc lc - market connection - ??
-                # 06.09. 24
+                # 06.09. 24_
            # lowcost = min(data)
             #highcost = max(data)
             #print("Наименьшая цена:", lowcost)
